944b69c25dfd528b6ec65dc1ff5b305998977a39
krosshykes <cks374@gmail.com>
2022-09-06 12:36:13 +0530

    Feat: Argument Parsing

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 	"os/exec"
 
 	"github.com/krosshykes/netonly/runas"
@@ -16,37 +16,53 @@ func main() {
 		path        string
 		curDir      string
 	)
-	user = "test"
-	logonDomain = "Domain"
-	pw = "test"
 
-	curDir = "C:\\users"
-	lenA := len(os.Args)
-	if lenA < 2 {
-		fmt.Println("Usage:")
-		fmt.Println("go run main.go username domain password appname directory")
-	} else if lenA < 3 {
-		switch os.Args[1] {
-		case "-h", "--help":
-			fmt.Println("Usage:")
-			fmt.Println("go run main.go username domain password appname directory")
-		default:
-			fmt.Println("Invalid Argument")
-		}
-	} else if lenA < 5 || lenA > 6 {
-		fmt.Println("Invalid number of Arguments")
+	flag.StringVar(&user, "u", "test", "Username")
+	flag.StringVar(&logonDomain, "d", "Domain", "Domain")
+	flag.StringVar(&pw, "p", "pass", "Password")
+	flag.StringVar(&path, "a", "powershell", "Application Name")
+	flag.StringVar(&curDir, "cd", "C:\\users", "Current Directory")
+	flag.Parse()
+	nPath, pErr := exec.LookPath(path)
+	if pErr != nil {
+		fmt.Println("Invalid Application Name: ", pErr)
 	} else {
-		user = os.Args[1]
-		logonDomain = os.Args[2]
-		pw = os.Args[3]
-		path, _ = exec.LookPath(os.Args[4])
-		if lenA == 6 {
-			curDir = os.Args[5]
-		}
-		err := runas.StartProcess(user, logonDomain, pw, path, curDir)
+		err := runas.StartProcess(user, logonDomain, pw, nPath, curDir)
 		if err != nil {
 			fmt.Println(err)
 		}
 	}
+	// user = "test"
+	// logonDomain = "Domain"
+	// pw = "test"
+
+	// curDir = "C:\\users"
+	// lenA := len(os.Args)
+	// if lenA < 2 {
+	// 	fmt.Println("Usage:")
+	// 	fmt.Println("go run main.go username domain password appname directory")
+	// } else if lenA < 3 {
+	// 	switch os.Args[1] {
+	// 	case "-h", "--help":
+	// 		fmt.Println("Usage:")
+	// 		fmt.Println("go run main.go username domain password appname directory")
+	// 	default:
+	// 		fmt.Println("Invalid Argument")
+	// 	}
+	// } else if lenA < 5 || lenA > 6 {
+	// 	fmt.Println("Invalid number of Arguments")
+	// } else {
+	// 	user = os.Args[1]
+	// 	logonDomain = os.Args[2]
+	// 	pw = os.Args[3]
+	// 	path, _ = exec.LookPath(os.Args[4])
+	// 	if lenA == 6 {
+	// 		curDir = os.Args[5]
+	// 	}
+	// 	err := runas.StartProcess(user, logonDomain, pw, path, curDir)
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
+	// }
 
 }
